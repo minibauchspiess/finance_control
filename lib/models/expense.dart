4c71d6470a984615fe0e531ae0841192f2ed55e0
Daniel Bauchspiess <dbauchspiess@gmail.com>
2023-02-13 07:57:44 -0300

    Added expense data structure

---
+++ lib/models/expense.dart
@@ -0,0 +1,29 @@
+import '../utils/expenses_data_base_handler.dart';
+
+class Expense {
+  String _expenseName = "";
+  double _initialValue = 0.0;
+  double _currentValue = 0.0;
+
+  Expense(this._expenseName, this._initialValue, this._currentValue);
+
+  void setExpenseName(String expenseName) {
+    _expenseName = expenseName;
+  }
+
+  void setInitialValue(double initialValue) {
+    _initialValue = initialValue;
+  }
+
+  void setCurrentValue(double currentValue) {
+    _currentValue = currentValue;
+  }
+
+  double get initialValue {
+    return _initialValue;
+  }
+
+  double get currentValue {
+    return _currentValue;
+  }
+}
