a160bf8fe0556cb521639e7350d2cc0a61e7b2e9
Daniel Bauchspiess <dbauchspiess@gmail.com>
2023-02-19 22:53:21 -0300

    Updated db name to be specific to expenses

---
+++ lib/utils/expenses_data_base_handler.dart
@@ -0,0 +1,77 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+import '../models/expense.dart';
+
+class ExpensesDb {
+  static const String tableName = 'ExpensesSources';
+  static const String expenseNameKey = 'expenseName';
+  static const String initialValueKey = 'initialValue';
+  static const String availableValueKey = 'availableValue';
+
+  static Future<sql.Database> _getDatabase() async {
+    final dbPath = await sql.getDatabasesPath();
+
+    final db = await sql.openDatabase(
+      path.join(dbPath, '$tableName.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          '''CREATE TABLE $tableName (
+                  $expenseNameKey TEXT PRIMARY KEY, 
+                  $initialValueKey REAL, 
+                  $availableValueKey REAL
+                )
+          ''',
+        );
+      },
+      version: 1,
+    );
+    return db;
+  }
+
+  static Future<List<String>> get expenseNames async {
+    final db = await ExpensesDb._getDatabase();
+    final namesMap =
+        await db.rawQuery('SELECT $expenseNameKey FROM $tableName');
+    final namesList =
+        namesMap.map((item) => item[expenseNameKey].toString()).toList();
+    return namesList;
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await ExpensesDb._getDatabase();
+
+    await db.insert(
+      table,
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await ExpensesDb._getDatabase();
+    return db.query(table);
+  }
+
+  static Future<List<Map<String, dynamic>>> getSingleData(
+      String expenseName) async {
+    final db = await ExpensesDb._getDatabase();
+
+    return db.rawQuery(
+        'SELECT * FROM $tableName WHERE $expenseNameKey = ?', [expenseName]);
+  }
+
+  static Future<void> updateAvailableValue(
+      String expenseName, double newValue) async {
+    final db = await _getDatabase();
+    await db.rawUpdate(
+        'UPDATE $tableName SET $availableValueKey = ? WHERE $expenseNameKey = ?',
+        [newValue, expenseName]);
+  }
+
+  static Future<void> delete(String table, String expenseName) async {
+    final db = await ExpensesDb._getDatabase();
+
+    await db.rawDelete(
+        'DELETE FROM $tableName WHERE $expenseNameKey = ?', [expenseName]);
+  }
+}
